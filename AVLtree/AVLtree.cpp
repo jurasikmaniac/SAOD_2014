359100605808176741d45daf89648e1736d2577d
Yurii <jurasikmaniac@gmail.com>
2014-04-26 20:55:48 +0700

    avltree

---
+++ AVLtree/AVLtree.cpp
@@ -0,0 +1,147 @@
+// AVLtree.cpp: определ€ет точку входа дл€ консольного приложени€.
+//
+
+#include "stdafx.h"
+#include <conio.h>
+
+#include <cmath>
+#include <cstdio>
+#include <ctype.h> 
+#include <clocale>
+#include <Windows.h>
+
+
+struct node // структура дл€ представлени€ узлов дерева
+{
+	int key;
+	unsigned char height;
+	node* left;
+	node* right;
+	node(int k) { key = k; left = right = 0; height = 1; }
+};
+
+unsigned char height(node* p)
+{
+	return p ? p->height : 0;
+}
+
+int bfactor(node* p)
+{
+	return height(p->right) - height(p->left);
+}
+
+void fixheight(node* p)
+{
+	unsigned char hl = height(p->left);
+	unsigned char hr = height(p->right);
+	p->height = (hl>hr ? hl : hr) + 1;
+}
+
+node* rotateright(node* p) // правый поворот вокруг p
+{
+	node* q = p->left;
+	p->left = q->right;
+	q->right = p;
+	fixheight(p);
+	fixheight(q);
+	return q;
+}
+
+node* rotateleft(node* q) // левый поворот вокруг q
+{
+	node* p = q->right;
+	q->right = p->left;
+	p->left = q;
+	fixheight(q);
+	fixheight(p);
+	return p;
+}
+
+node* balance(node* p) // балансировка узла p
+{
+	fixheight(p);
+	if (bfactor(p) == 2)
+	{
+		if (bfactor(p->right) < 0)
+			p->right = rotateright(p->right);
+		return rotateleft(p);
+	}
+	if (bfactor(p) == -2)
+	{
+		if (bfactor(p->left) > 0)
+			p->left = rotateleft(p->left);
+		return rotateright(p);
+	}
+	return p; // балансировка не нужна
+}
+
+node* insert(node* p, int k) // вставка ключа k в дерево с корнем p
+{
+	if (!p) return new node(k);
+	if (k<p->key)
+		p->left = insert(p->left, k);
+	else
+		p->right = insert(p->right, k);
+	return balance(p);
+}
+
+node* findmin(node* p) // поиск узла с минимальным ключом в дереве p 
+{
+	return p->left ? findmin(p->left) : p;
+}
+
+void find(node* p, int k)
+{
+	if (p)
+	{
+		if (p->key == k) { _putch((char)k); find(p->right, k); }
+		else
+		if (p->key < k) find(p->right, k); 
+		else find(p->left, k);
+
+	}
+}
+node* removemin(node* p) // удаление узла с минимальным ключом из дерева p
+{
+	if (p->left == 0)
+		return p->right;
+	p->left = removemin(p->left);
+	return balance(p);
+}
+
+node* remove(node* p, int k) // удаление ключа k из дерева p
+{
+	if (!p) return 0;
+	if (k < p->key)
+		p->left = remove(p->left, k);
+	else if (k > p->key)
+		p->right = remove(p->right, k);
+	else //  k == p->key 
+	{
+		node* q = p->left;
+		node* r = p->right;
+		delete p;
+		if (!r) return q;
+		node* min = findmin(r);
+		min->right = removemin(r);
+		min->left = q;
+		return balance(min);
+	}
+	return balance(p);
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	node *root = NULL;
+	for (size_t i = 0; i < 7; i++)
+	{
+
+		root = insert(root, (int)i);
+	}
+	root = insert(root, 5);
+	root = insert(root, 5);
+	find(root,5);
+	system("pause");
+	return 0;
+}
+
